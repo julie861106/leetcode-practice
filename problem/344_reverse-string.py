166f0bcfa6e62c9e4f2ff97e3a68ecb435c12cd3
julie861106 <julieyao1106@gmail.com>
2024-06-02 23:48:24 +0800

    ✨ feat: 344_reverse-string done

--- problem/344_reverse-string.py
+++ problem/344_reverse-string.py
@@ -5,3 +5,18 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        len_s = len(s)
+        for i in range(len_s//2):
+            s[i], s[len_s-1-i] = s[len_s-1-i], s[i]
+        return s
+
+test = Solution()
+
+test_set = [["h","e","l","l","o"], ["H","a","n","n","a","h"]]
+test_ans = [["o","l","l","e","h"], ["h","a","n","n","a","H"]]
+
+for num, test_string in enumerate(test_set):
+    if test_ans[num] != test.reverseString(test_string):
+        print(f"FAILED: Answer is {test_ans[num]} NOT {test.reverseString(test_string)}")
+    else:
+        print("PASSED")
