16180805c2f7e0c8d04bc17958afd88a9247eb3f
julie861106 <julieyao1106@gmail.com>
2024-06-03 21:36:26 +0800

    test: 💍 pytest for 2486 append char to make subsequence

--- testcase/test_string.py
+++ testcase/test_string.py
@@ -1,5 +1,5 @@
 import pytest
-from problem.reverse_string import Solution
+from problem import reverse_string, append_characters
 
 
 class TestString:
@@ -9,6 +9,22 @@ class TestString:
         test_set = [["h","e","l","l","o"], ["H","a","n","n","a","h"]]
         test_ans = [["o","l","l","e","h"], ["h","a","n","n","a","H"]]
 
-        test = Solution()
+        test = reverse_string.Solution()
         for num, test_string in enumerate(test_set):
             assert test.reverseString(test_string) == test_ans[num]
+
+    @pytest.mark.string
+    @pytest.mark.test_key("2486")
+    def test_appendCharacters(self):
+        test_set = [
+            ["coaching", "coding"],
+            ["abcde", "a"],
+            ["z", "abcde"],
+            ["abcab", "cbba"]
+            ]
+        test_ans = [4, 0, 5, 2]
+
+        test = append_characters.Solution()
+
+        for num, test_case in enumerate(test_set):
+            assert test_ans[num] == test.appendCharacters(test_case[0], test_case[1])
