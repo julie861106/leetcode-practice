7221ef3ef233807c09c9464d8608bbd6c780b275
julie861106 <julieyao1106@gmail.com>
2024-06-07 21:27:57 +0800

    ✨ feat: 240607 daily question

---
+++ problem/replace_words.py
@@ -0,0 +1,10 @@
+
+from typing import List
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        """
+        648. Replace Words
+        https://leetcode.com/problems/replace-words/description
+        """
+
+        pass
